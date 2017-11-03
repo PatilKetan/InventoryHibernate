8a38a40aad1ceb5b0b91d1adff5c016c5caddc4b
Ketan Patil <ketan.patil@neptune-ubi.com>
2017-11-03 17:32:16 +0530

    setup 2.0

--- src/main/java/org/ketan/inventory/Inventory.java
+++ src/main/java/org/ketan/inventory/Inventory.java
@@ -1,5 +1,8 @@
 package org.ketan.inventory;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,16 +10,37 @@ import org.ketan.model.Item;
 
 public class Inventory {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Item item = new Item();
+		Item item1 = new Item();
+		Item item2 = new Item();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
 		item.setItemId(0);
-		item.setItemName("ItemOne");
+		item.setItemName("juice");
+		item.setManufacturedDate(sdf.parse("2014-09-11"));
+		item.setEntryDate(sdf.parse("2014-12-21"));
+		item.setExpiryDate(sdf.parse("2015-09-11"));
+		
+		item1.setItemId(1);
+		item1.setItemName("cornflakes");
+		item1.setManufacturedDate(sdf.parse("2014-04-10"));
+		item1.setEntryDate(sdf.parse("2014-05-01"));
+		item1.setExpiryDate(sdf.parse("2015-04-10"));
+		
+		item2.setItemId(2);
+		item2.setItemName("lays");
+		item2.setManufacturedDate(sdf.parse("2014-09-11"));
+		item2.setEntryDate(sdf.parse("2015-12-21"));
+		item2.setExpiryDate(sdf.parse("2016-09-11"));
 		
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		Session session = factory.openSession();
 		session.beginTransaction();
 		
 		session.save(item);
+		session.save(item1);
+		session.save(item2);
 		
 		session.getTransaction().commit();
 
--- src/main/java/org/ketan/model/Item.java
+++ src/main/java/org/ketan/model/Item.java
@@ -1,21 +1,47 @@
 package org.ketan.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Item {
 	
 	@Id
+	@Column(name = "Item_Id")
 	private int itemId;
+	
+	@Column(name = "Item_Name")
 	private String itemName;
 	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "Manufactured_Date")
+	private Date manufacturedDate;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "Entry_Date")
+	private Date entryDate;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "Expiry_Date")
+	private Date expiryDate;
+	
+
 	public Item() {}
 	
-	public Item(int itemId, String itemName) {
+	public Item(int itemId, String itemName, Date manufacturedDate, Date entryDate, Date expiryDate) {
 		this.itemId = itemId;
 		this.itemName = itemName;
+		this.manufacturedDate = manufacturedDate;
+		this.entryDate = entryDate;
+		this.expiryDate = expiryDate;
 	}
+
+
 	public int getItemId() {
 		return itemId;
 	}
@@ -27,6 +53,24 @@ public class Item {
 	}
 	public void setItemName(String itemName) {
 		this.itemName = itemName;
+	}
+	public Date getManufacturedDate() {
+		return manufacturedDate;
+	}
+	public void setManufacturedDate(Date manufacturedDate) {
+		this.manufacturedDate = manufacturedDate;
+	}
+	public Date getEntryDate() {
+		return entryDate;
+	}
+	public void setEntryDate(Date entryDate) {
+		this.entryDate = entryDate;
+	}
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
 	}
 	
 }
